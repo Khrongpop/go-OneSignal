b38a5cd0c394b0c39c4278b08b57c1649829ac61
Khrongpop <mr.khrongpop@gmail.com>
2019-03-27 02:12:17 +0700

    port

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"html"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -12,32 +14,32 @@ import (
 func main() {
 	viper.AutomaticEnv()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// url := "https://onesignal.com/api/v1/notifications"
-		// fmt.Println("URL:>", url)
-		// viper.AutomaticEnv()
-		// appID := viper.GetString("OSAPPID")
-		// APIKEY := viper.GetString("OSAPIKEY")
-		// var jsonStr = []byte(`{
-		// 	"app_id": "` + appID + `",
-		// 	"include_player_ids": ["3ca9a849-bdf7-4986-afea-89bf18c94b6b"],
-		// 	"data": {"foo": "bar"},
-		// 	"contents": {"en": "kuyyyyy2323"}
-		//   }`)
-		// req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-		// req.Header.Set("Authorization", "Basic "+APIKEY)
-		// req.Header.Set("Content-Type", "application/json; charset=utf-8")
+		url := "https://onesignal.com/api/v1/notifications"
+		fmt.Println("URL:>", url)
+		viper.AutomaticEnv()
+		appID := viper.GetString("OSAPPID")
+		APIKEY := viper.GetString("OSAPIKEY")
+		var jsonStr = []byte(`{
+			"app_id": "` + appID + `",
+			"include_player_ids": ["3ca9a849-bdf7-4986-afea-89bf18c94b6b"],
+			"data": {"foo": "bar"},
+			"contents": {"en": "kuyyyyy2323"}
+		  }`)
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+		req.Header.Set("Authorization", "Basic "+APIKEY)
+		req.Header.Set("Content-Type", "application/json; charset=utf-8")
 
-		// client := &http.Client{}
-		// resp, err := client.Do(req)
-		// if err != nil {
-		// 	panic(err)
-		// }
-		// defer resp.Body.Close()
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
 
-		// fmt.Println("response Status:", resp.Status)
-		// fmt.Println("response Headers:", resp.Header)
-		// body, _ := ioutil.ReadAll(resp.Body)
-		// fmt.Println("response Body:", string(body))
+		fmt.Println("response Status:", resp.Status)
+		fmt.Println("response Headers:", resp.Header)
+		body, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println("response Body:", string(body))
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
